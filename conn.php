22cb482051953f1d8883d4a3f8a20cdcb4bbc7ea
Eka Febriansah <febriye12@gmail.com>
2022-02-06 19:34:04 +0700

    Tugas-Akhir

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $databasename = "latihanapi";
+    $databasename = "tugasakhir";
 
     $conn = mysqli_connect($servername, $username, $password, $databasename);
     if(!$conn){
